9d9a1f573ee7cec0742da12d7e3db65c93927838
ymsht <ymshttts@gmail.com>
2021-06-12 16:48:10 +0900

    nullを許可

--- model/art.go
+++ model/art.go
@@ -2,24 +2,24 @@ package model
 
 import (
 	"database/sql"
-	"github.com/go-sql-driver/mysql"
+
 	"gopkg.in/gorp.v1"
 )
 
 // Art 芸術情報
 type Art struct {
-	ArtName              string    `db:"art_name" json:"art_name"`
-	ArtNameEn            string    `db:"art_name_en" json:"art_name_en"`
-	YearOfProductionFrom mysql.NullTime `db:"year_of_production_from" json:"year_of_production_from"`
-	YearOfProductionTo   mysql.NullTime `db:"year_of_production_to" json:"year_of_production_to"`
-	Evaluation           float32   `db:"evaluation" json:"evaluation"`
-	Height               float32   `db:"height" json:"height"`
-	Width                float32   `db:"width" json:"width"`
-	FileUrl              string    `db:"file_url" json:"json"`
-	AuthorName           sql.NullString    `db:"author_name" json:"author_name"`
-	SuppliesName         sql.NullString    `db:"supplies_name" json:"supplies_name"`
-	CollectionName       sql.NullString    `db:"collection_name" json:"collection_name"`
-	PaintName            sql.NullString    `db:"paint_name" json:"paint_name"`
+	ArtName              string         `db:"art_name" json:"art_name"`
+	ArtNameEn            sql.NullString `db:"art_name_en" json:"art_name_en"`
+	YearOfProductionFrom sql.NullString `db:"year_of_production_from" json:"year_of_production_from"`
+	YearOfProductionTo   sql.NullString `db:"year_of_production_to" json:"year_of_production_to"`
+	Evaluation           float32        `db:"evaluation" json:"evaluation"`
+	Height               float32        `db:"height" json:"height"`
+	Width                float32        `db:"width" json:"width"`
+	FileUrl              string         `db:"file_url" json:"file_url"`
+	AuthorName           sql.NullString `db:"author_name" json:"author_name"`
+	SuppliesName         sql.NullString `db:"supplies_name" json:"supplies_name"`
+	CollectionName       sql.NullString `db:"collection_name" json:"collection_name"`
+	PaintName            sql.NullString `db:"paint_name" json:"paint_name"`
 }
 
 // GetArts 芸術情報を検索します
@@ -41,8 +41,8 @@ func selectToArt(tx *gorp.Transaction) ([]Art, error) {
 select 
   art.name art_name, 
   art.name_en art_name_en, 
-  art.year_of_production_from, 
-  art.year_of_production_to, 
+  DATE_FORMAT(art.year_of_production_from, '%Y') year_of_production_from,
+  DATE_FORMAT(art.year_of_production_to, '%Y') year_of_production_to,
   art.evaluation, 
   art.height, 
   art.width, 
